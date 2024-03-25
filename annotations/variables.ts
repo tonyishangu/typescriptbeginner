fbd881f270c372afa79d4446bb3a7f0a74ccbee1
tonyishangu <tonyishangu@gmail.com>
2024-03-25 16:21:11 +0300

    object literal annotations

--- annotations/variables.ts
+++ annotations/variables.ts
@@ -3,3 +3,23 @@ let speed: string = 'fast'
 let hasName: boolean = true
 let nothingMuch: null = null
 let legendsOfHeroes: undefined = undefined
+
+// built in objects
+
+let now: Date = new Date()
+
+// arrays
+let colors: string[] = ['red', 'blue', 'green']
+let  numbers: number[] = [1, 2, 3]
+
+// classes
+class Car {
+
+}
+let car: Car = new Car()
+
+// object literal
+let point: { x: number; y:number} = {
+  x: 90,
+  y: 20
+}
